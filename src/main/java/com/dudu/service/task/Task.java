b5136ae9189796276b0224da3c31b4502f9c5df5
dingchun <dingchun_sky@163.com>
2020-10-26 15:49:25 +0800

    fix

---
+++ src/main/java/com/dudu/service/task/Task.java
@@ -0,0 +1,29 @@
+package com.dudu.service.task;
+
+import com.dudu.dao.redis.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+@EnableAsync
+public class Task {
+    public static Logger log = LoggerFactory.getLogger(RedisUtil.class);
+
+    @Async
+    @Scheduled(cron = "1 * * * * *")
+    public void taskone() {
+        log.info("taskone");
+    }
+
+    @Async
+    @Scheduled(fixedDelay = 1000)
+    public void tasktow() {
+        log.info("tasktwo");
+    }
+}
