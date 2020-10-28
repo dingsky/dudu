f83b3d0356cd7d00103143068afb94e3999db9f9
dingchun <dingchun_sky@163.com>
2020-10-28 18:58:42 +0800

    fix

--- src/main/java/com/dudu/config/UserConfig.java
+++ src/main/java/com/dudu/config/UserConfig.java
@@ -8,22 +8,22 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class UserConfig {
-    @NacosValue("${spring.datasource.url}")
-    private String url;
-
-    @NacosValue("${spring.datasource.username}")
-    private String username;
-
-    @NacosValue("${spring.datasource.password}")
-    private String password;
-
-    @Primary
-    @Bean
-    public DataSourceProperties dataSourceProperties() {
-        DataSourceProperties dataSourceProperties = new DataSourceProperties();
-        dataSourceProperties.setUrl(url);
-        dataSourceProperties.setPassword(password);
-        dataSourceProperties.setUsername(username);
-        return dataSourceProperties;
-    }
+//    @NacosValue("${spring.datasource.url}")
+//    private String url;
+//
+//    @NacosValue("${spring.datasource.username}")
+//    private String username;
+//
+//    @NacosValue("${spring.datasource.password}")
+//    private String password;
+//
+//    @Primary
+//    @Bean
+//    public DataSourceProperties dataSourceProperties() {
+//        DataSourceProperties dataSourceProperties = new DataSourceProperties();
+//        dataSourceProperties.setUrl(url);
+//        dataSourceProperties.setPassword(password);
+//        dataSourceProperties.setUsername(username);
+//        return dataSourceProperties;
+//    }
 }
--- src/test/java/com/dudu/DuduApplicationTests.java
+++ src/test/java/com/dudu/DuduApplicationTests.java
@@ -1,13 +1,21 @@
 package com.dudu;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@SpringBootTest(classes = DuduApplication.class)
 class DuduApplicationTests {
+    @Resource
+    DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception{
+        System.out.println(dataSource.getConnection());
     }
 
 }
